988b9dc6118d64a36ea4983bbb3b1e701065dd9f
isabellexvr <isabellexavier59@gmail.com>
2023-02-04 20:13:11 -0300

    feat: seed creates hotel and rooms

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,6 +15,32 @@ async function main() {
       },
     });
   }
+  let hotel = await prisma.hotel.findFirst();
+  if (!hotel) {
+    hotel = await prisma.hotel.create({
+      data: {
+        id: 1,
+        name: "Plaza Drivent",
+        image: "https://img.freepik.com/vetores-gratis/fundo-de-fachada-plana-hotel_23-2148157379.jpg?w=2000"
+      },
+    });
+    await prisma.room.createMany({
+      data: [
+        {
+        id: 1,
+        name: "Quarto 1",
+        capacity: 4,
+        hotelId: 1
+      },
+      {
+        id: 2,
+        name: "Quarto 2",
+        capacity: 3,
+        hotelId: 1
+      }
+    ]
+    })
+  }
 
   console.log({ event });
 }
